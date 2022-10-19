19202d159e07f8551e5f65a11554f56cf84424f2
Jocelyn <jocelyntan89@hotmail.com>
2022-10-19 17:34:23 +0200

    setup global styles

---
+++ src/assets/styles/theme.js
@@ -0,0 +1,19 @@
+export default {
+  colors: {
+    black: '#202020',
+    white: '#FFFFF'
+  },
+  fontSize: {
+    large: '220px',
+    regular: '16px',
+    small: '10px'
+  },
+  fontWeight: {
+    bold: 700,
+    regular: 400
+  },
+  borderRadius: {
+    button: '24px',
+    image: '10px'
+  }
+}
