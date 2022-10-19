4b5ff192569f7c498284bfb73b88d8f4cce2c6e1
Jocelyn <jocelyntan89@hotmail.com>
2022-10-19 18:56:26 +0200

    add data

---
+++ src/data/index.js
@@ -0,0 +1,77 @@
+export default [
+  {
+    image: {
+      mobile: 'image01.jpg',
+      desktop: 'image01@2x.jpg',
+      alt: ''
+    },
+    headline: 'Everyday Flowers',
+    author: 'Johanna Hobel for Vouge',
+    date: 'Jun 2019',
+    button: {
+      label: 'Have a look',
+      href: '',
+      target: ''
+    }
+  },
+  {
+    image: {
+      mobile: 'image02.jpg',
+      desktop: 'image02@2x.jpg',
+      alt: ''
+    },
+    headline: 'The Wilder Night',
+    author: 'Johanna Hobel for Wild',
+    date: 'Dec 2019',
+    button: {
+      label: 'Have a look',
+      href: '',
+      target: ''
+    }
+  },
+  {
+    image: {
+      mobile: 'image03.jpg',
+      desktop: 'image03@2x.jpg',
+      alt: ''
+    },
+    headline: 'Smooth Memories',
+    author: 'Johanna Hobel for Chanel',
+    date: 'Feb 2020',
+    button: {
+      label: 'Have a look',
+      href: '',
+      target: ''
+    }
+  },
+  {
+    image: {
+      mobile: 'image04.jpg',
+      desktop: 'image04@2x.jpg',
+      alt: ''
+    },
+    headline: 'The Future Universe',
+    author: 'Johanna Hobel for On',
+    date: 'Apr 2020',
+    button: {
+      label: 'Have a look',
+      href: '',
+      target: ''
+    }
+  },
+  {
+    image: {
+      mobile: 'image05.jpg',
+      desktop: 'image05@2x.jpg',
+      alt: ''
+    },
+    headline: 'She was born urban',
+    author: 'Johanna Hobel for Si',
+    date: 'Dec 2021',
+    button: {
+      label: 'Have a look',
+      href: '',
+      target: ''
+    }
+  }
+]
